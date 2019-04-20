5f8d4281cfef9851c579b7b97efd9401aa0e59ad
Saman Maharjan <onepiece.maharjan@gmail.com>
2019-04-20 14:38:05 +0545

    added a click function

--- js/main.js
+++ js/main.js
@@ -272,14 +272,14 @@
             .find(".block2-name")
             .html();
         $(this).on("click", function () {
-            debugger;
+            // debugger;
             var img = $(this)
                 .parent()
                 .parent()
                 .parent()
                 .find(".image")
                 .attr("src");
-            console.log(img);
+            // console.log(img);
             // swal(nameProduct, "is added to cart !", "success");
             swal({
                 icon: img,
@@ -315,49 +315,156 @@
             .html();
         $(this).on("click", function () {
             swal(nameProduct, "will be delivered to you soon!", "success");
-
             // swal(nameProduct, "is added to wishlist !", "success");
         });
     });
 
-    $(".form_phone").keyup(function () {
-        var total = 10;
-        var this_number = $(this).val();
-        // debugger
-        print(this_number);
-        if (total == this_number) {
-            alert("okey number");
+    $(".add-to-cart").on("click", function () {
+        debugger
+        var name = $(this).closest('.item-slick2').find('.product-detail-name').text();
+        var price = $(this).closest('.item-slick2').find('.product-detail-price').text();
+        var image = $(this).closest('.item-slick2').find('.product-detail-image').attr("src");
+        var qty = $(".header-icons").find(".header-icons-noti").text();
+
+        // image = "images/item-cart-02.jpg";
+        console.log(name.trimStart() + '   ' + price.trim());
+
+        var myCart = "<li class='header-cart-item'>" +
+            "<div class = 'header-cart-item-img' >" +
+            "<img src = " + image + " alt = 'IMG' >" +
+            "</div>" +
+            "<div class = 'header-cart-item-txt' >" +
+            "<a href = '#' class = 'header-cart-item-name' >" +
+            name +
+            "</a>" + "<span class = 'header-cart-item-info' > " +
+            price + "</span>" + "</div >" + "</li>";
+        $(".header-cart-wrapitem").append(myCart);
+
+        $(".header-icons").find(".header-icons-noti").text(1);
+
+        var total = $(".header-icons").find(".header-cart-total").text();
+        total.replace("Total: Rs ", "0");
+        console.log(total);
+        total = "Total: " + price;
+        $(".header-icons").find(".header-cart-total").text(total);
+
+
+
+
+        console.log("name=" + name + " qunatity=" + qty + " price=" + price + " total=" + total + " image=" + image);
+        var myObjs = [];
+        var myObj = {
+            "name": name,
+            "qty": qty,
+            "price": price,
+            "total": total,
+            "image": image
+        };
+
+        myObjs.push(myObj)
+        console.log("this is my objects" + myObjs);
+        var myJSON = JSON.stringify(myObjs);
+        console.log("stringyfied= " + myJSON);
+        //		myJSON.push(myJSON);
+        localStorage.setItem("sentJSON", myJSON);
+        console.log("length in local storage=" + localStorage.length);
+
+        //Retrieving data:
+        var text = localStorage.getItem("sentJSON");
+        var obj = JSON.parse(text);
+        //console.log(obj);
+        debugger
+        document.getElementsByClassName("show_name").text = obj.name;
+        console.log(obj.length);
+        var tot = 0;
+        for (i = 0; i < obj.length; i++) {
+            var addDivs = "<div class='row table'>" + "<div class='col-lg-3 show_name'>" +
+                obj[i].qty + "</div>" + "<div class = 'col-lg-3 show_price ' >" + obj[i].price +
+                " </div>" + " <div class = 'col-lg-3 show_image' > " +
+                "<img src='" + obj[i].image + "' width='100px' height='100px' alt='no image'>" + "</div>" + "</div>"
+            $('.cartList').append(addDivs);
+            tot += obj[i].total;
+            console.log(tot);
+            $('.set_total').text(obj[i].total);
         }
+        $('.set_total').text(tot);
+
+
     });
 
-    $(function () {
-        $(".gform").validate({
-            rules: {
-                phone: {
-                    required: true,
-                    range: [10, 10]
-                }
-            }
-        });
-    });
+    //Retrieving data:
+    var text = localStorage.getItem("sentJSON");
+    var obj = JSON.parse(text);
+    console.log(obj);
+    console.log(obj.length);
+    for (i = 0; i < obj.length; i++) {
 
-    function getValidNumber(value) {
-        value = $.trim(value).replace(/\D/g, "");
+        var addDiv = "		<tr class='table-row'>" +
+            "<td class = 'column-1' >" +
+            "<div class = 'cart-img-product b-rad-4 o-f-hidden' >" +
+            "<img src = " + obj[i].image + " alt = 'IMG - PRODUCT ' >" +
+            "</div>" + "</td>" +
+            "<td class = 'column-2' >" + obj[i].name + "</td>" +
+            "<td class = 'column-3' >" + obj[i].price + "</td>" +
+            "<td class = 'column-4' >" + "<div class = 'flex-w bo5 of-hidden w-size17' >" +
+            "<button class = 'btn-num-product-down color1 flex-c-m size7 bg8 eff2' >" +
+            "<i class = 'fs-12 fa fa-minus' aria-hidden = 'true' >" + "</i>" + " </button>" +
 
-        if (value.substring(0, 1) == "1") {
-            value = value.substring(1);
-        }
+            "<input class = 'size8 m-text18 t-center num-product' type = 'number' name = 'num-product1' value = " + obj[i].qty + " >" +
+            "<button class = 'btn-num-product-up color1 flex-c-m size7 bg8 eff2' >" +
+            "<i class = 'fs-12 fa fa-plus' aria-hidden = 'true' >" + "</i>" + "</button >" + "</div>" + "</td>" +
+            "<td class = 'column-5' >" + obj[i].total + "</td> </tr > ";
 
-        if (value.length == 10) {
-            return value;
-        }
-
-        return false;
+        var addDivs = "<div class='row table'>" + "<div class='col-lg-3 show_name'>" +
+            obj[i].qty + "</div>" + "<div class = 'col-lg-3 show_price ' >" + obj[i].price +
+            " </div>" + " <div class = 'col-lg-3 show_image' > " +
+            "<img src='" + obj[i].image + "' width='100px' height='100px' alt='no image'>" + "</div>" + "</div>"
+        $('.table-shopping-cart').append(addDiv);
+        tot += obj[i].total;
+        console.log(tot);
+        $('.set_total').text(obj[i].total);
     }
-    var num = getValidNumber("(123) 456-7890");
-    if (num !== false) {
-        alert("The valid number is: " + num);
-    } else {
-        alert("The number you passed is not a valid US phone number");
-    }
+    $('.cart-total').text(obj.total);
+
+
+    // $(".form_phone").keyup(function () {
+    //     var total = 10;
+    //     var this_number = $(this).val();
+    // debugger
+    //     print(this_number);
+    //     if (total == this_number) {
+    //         alert("okey number");
+    //     }
+    // });
+
+    // $(function () {
+    //     $(".gform").validate({
+    //         rules: {
+    //             phone: {
+    //                 required: true,
+    //                 range: [10, 10]
+    //             }
+    //         }
+    //     });
+    // });
+
+    // function getValidNumber(value) {
+    //     value = $.trim(value).replace(/\D/g, "");
+
+    //     if (value.substring(0, 1) == "1") {
+    //         value = value.substring(1);
+    //     }
+
+    //     if (value.length == 10) {
+    //         return value;
+    //     }
+
+    //     return false;
+    // }
+    // var num = getValidNumber("(123) 456-7890");
+    // if (num !== false) {
+    //     alert("The valid number is: " + num);
+    // } else {
+    //     alert("The number you passed is not a valid US phone number");
+    // }
 })(jQuery);
